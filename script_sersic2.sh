19a968779bdefa9931316eab46c86bcb74ef2579
eli <elizabeth.johana.gonzalez@gmail.com>
2020-07-14 18:10:09 -0300

    run again

---
+++ script_sersic2.sh
@@ -0,0 +1,3 @@
+srun python -u forGroup_profile.py -sample 'N4M_Mbin1'  -N_min 4 -N_max 100 -lMH_min 12.5 -lMH_max 13.5 -C_BG_min 0. -z_max 0.2 -ncores 48
+srun python -u forGroup_profile.py -sample 'N4M_Mbin2'  -N_min 4 -N_max 100 -lMH_min 13.5 -lMH_max 14.0 -C_BG_min 0. -z_max 0.2 -ncores 48
+srun python -u forGroup_profile.py -sample 'N4M_Mbin3'  -N_min 4 -N_max 100 -lMH_min 14.0 -lMH_max 15.5 -C_BG_min 0. -z_max 0.2 -ncores 48
