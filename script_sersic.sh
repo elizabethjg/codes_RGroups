570059803bfe654357dedafee6041e4dddd85e71
eli <elizabeth.johana.gonzalez@gmail.com>
2020-07-14 19:26:21 -0300

    run again

--- script_sersic.sh
+++ script_sersic.sh
@@ -1,3 +1,3 @@
-python -u forGroup_profile.py -sample 'N23_Mbin1'  -N_min 2 -N_max 4 -lMH_min 12.5 -lMH_max 13.3  -C_BG_min 0. -z_max 0.2 -ncores 30
-python -u forGroup_profile.py -sample 'N23_Mbin2'  -N_min 2 -N_max 4 -lMH_min 13.3 -lMH_max 13.6  -C_BG_min 0. -z_max 0.2 -ncores 30
-python -u forGroup_profile.py -sample 'N23_Mbin3'  -N_min 2 -N_max 4 -lMH_min 13.6 -lMH_max 14.5  -C_BG_min 0. -z_max 0.2 -ncores 30
+python -u forGroup_profile.py -sample 'N23_Mbin1_cM'  -N_min 2 -N_max 4 -lMH_min 12.5 -lMH_max 13.3  -C_BG_min 3. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'N23_Mbin2_cM'  -N_min 2 -N_max 4 -lMH_min 13.3 -lMH_max 13.6  -C_BG_min 3. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'N23_Mbin3_cM'  -N_min 2 -N_max 4 -lMH_min 13.6 -lMH_max 14.5  -C_BG_min 3. -z_max 0.2 -ncores 48
