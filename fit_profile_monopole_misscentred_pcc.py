f2fb0cbfb6266f383084661045935c397b78cc24
eli <elizabeth.johana.gonzalez@gmail.com>
2020-09-07 16:37:33 -0300

    compute again

--- fit_profile_monopole_misscentred_pcc.py
+++ fit_profile_monopole_misscentred_pcc.py
@@ -98,7 +98,7 @@ maskr = (p.Rp < ROUT)
 t1 = time.time()
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, 
                                 args=(p.Rp[maskr],p.DSigma_T[maskr],p.error_DSigma_T[maskr]))
-sampler.run_mcmc(pos, 300, progress=True)
+sampler.run_mcmc(pos, 100, progress=True)
 print '//////////////////////'
 print '         TIME         '
 print '----------------------'
