d11cc3d8624f2f0ac7f05a9728f12d0aed1abb1a
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2020-11-30 12:47:04 -0300

    run ind cats

--- script_sersic2.sh
+++ script_sersic2.sh
@@ -1,7 +1,7 @@
-python -u forGroup_profile.py -sample 'Mbin1_CS82'  -lMH_min 12.5 -lMH_max 13.0 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin2_CS82'  -lMH_min 13.0 -lMH_max 13.3 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin3_CS82'  -lMH_min 13.3 -lMH_max 13.6 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin4_CS82'  -lMH_min 13.6 -lMH_max 13.9 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin5_CS82'  -lMH_min 13.9 -lMH_max 14.2 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin6_CS82'  -lMH_min 14.2 -lMH_max 14.5 -C_BG_min 0. -z_max 0.2 -ncores 56
-python -u forGroup_profile.py -sample 'Mbin7_CS82'  -lMH_min 14.5 -lMH_max 15.0 -C_BG_min 0. -z_max 0.2 -ncores 56
+python -u forGroup_profile.py -sample 'Mbin1_CS82'  -lMH_min 12.5 -lMH_max 13.0 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin2_CS82'  -lMH_min 13.0 -lMH_max 13.3 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin3_CS82'  -lMH_min 13.3 -lMH_max 13.6 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin4_CS82'  -lMH_min 13.6 -lMH_max 13.9 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin5_CS82'  -lMH_min 13.9 -lMH_max 14.2 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin6_CS82'  -lMH_min 14.2 -lMH_max 14.5 -C_BG_min 0. -z_max 0.2 -ncores 48
+python -u forGroup_profile.py -sample 'Mbin7_CS82'  -lMH_min 14.5 -lMH_max 15.0 -C_BG_min 0. -z_max 0.2 -ncores 48
